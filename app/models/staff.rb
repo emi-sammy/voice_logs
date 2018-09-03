d9638a53ff7870b1363b9afe4a4cb27ba0c1ed45
“oikawaemi” <emi-sammy@joy.ocn.ne>
2018-09-03 21:58:48 +0900

    “404ページ処理”

--- app/models/staff.rb
+++ app/models/staff.rb
@@ -1,3 +1,5 @@
 class Staff < ApplicationRecord
 
+   validates :staff_num,  presence: true, length: { maximum: 10 }
+
 end
--- app/models/voice_check.rb
+++ app/models/voice_check.rb
@@ -1,4 +1,4 @@
 class VoiceCheck < ApplicationRecord
 
-
+ validates :staff_num,  presence: true, length: { maximum: 10 }
 end
