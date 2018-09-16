874aeb06c7ffbb389f523bb5950b25e050cbce41
“oikawaemi” <emi-sammy@joy.ocn.ne>
2018-09-16 13:27:19 +0900

    “top画像更新”

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,9 +28,9 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  # herokuに画像を読み込ませるため追記変更→config.assets.compile = false 
-  config.assets.compile = true
-  config.serve_static_assets = true
+  # herokuに画像を読み込ませるため追記変更→config.assets.compile = false
+  config.assets.compile = false
+  #config.serve_static_assets = true
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
